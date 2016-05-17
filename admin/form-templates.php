17ed395f61db9908b138c2edaa99a428e0008f60
mithu <cemithu06@gmail.com>
2016-05-17 10:11:24 +0600

    form settings added

--- admin/form-templates.php
+++ admin/form-templates.php
@@ -23,6 +23,73 @@ class WPUF_Form_Templates{
         )
     );
 
+    public static $form_settings = array(
+        'post_form' => array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'post_format' => 0,
+            'default_cat' => -1,
+            'guest_post' => false,
+            'name_label' => 'Name',
+            'email_label' => 'Email',
+            'message_restrict' => 'This page is restricted. Please Log in / Register to view this page.',
+            'redirect_to' => 'post',
+            'message' => 'Post saved',
+            'page_id' => 81,
+            'url' => '',
+            'comment_status' => 'open',
+            'submit_text' => 'Submit',
+            'draft_post' => 'true',
+            'multistep_progressbar_type' => 'step_by_step',
+            'ms_ac_txt_color' => '#ffffff',
+            'ms_active_bgcolor' => '#00a0d2',
+            'ms_bgcolor' => '#E4E4E4',
+            'edit_post_status' => 'publish',
+            'edit_redirect_to' => 'same',
+            'update_message' => 'Post updated successfully',
+            'edit_page_id' => 81,
+            'edit_url' =>,
+            'subscription' => '- Select -',
+            'update_text' => 'Update',
+            'notification' => Array
+                (
+                    'new' => 'on',
+                    'new_to' => 'a@a.a',
+                    'new_subject' => 'New post created',
+                    'new_body' => 'Hi Admin,
+A new post has been created in your site %sitename% (%siteurl%).
+
+Here is the details:
+Post Title: %post_title%
+Content: %post_content%
+Author: %author%
+Post URL: %permalink%
+Edit URL: %editlink%',
+                    'edit' => 'off',
+                    'edit_to' => 'a@a.a',
+                    'edit_subject' => 'A post has been edited',
+                    'edit_body' => 'Hi Admin,
+The post \"%post_title%\" has been updated.
+
+Here is the details:
+Post Title: %post_title%
+Content: %post_content%
+Author: %author%
+Post URL: %permalink%
+Edit URL: %editlink%'
+                ),
+            'expiration_settings' => Array
+            (
+                'enable_post_expiration' => 'on',
+                'expiration_time_value' => 1,
+                    'expiration_time_type' => 'day',
+                    'expired_post_status' => 'draft',
+                    'post_expiration_message' => ''
+                )
+
+        )
+    );
+
     public static $elements_attr;
 
     public static function fill_data( $post_id,  $form_type ){
